56e3a8a3e83c642b3846b28fbce54cd3da524de3
germandus <germandouce@gmail.com>
2021-07-14 20:30:25 -0300

    funcion de creacion de consulta (no funca todavia!)

--- funcionalidades_drive.py
+++ funcionalidades_drive.py
@@ -17,29 +17,29 @@ def validar_opcion(opc_minimas: int, opc_maximas: int, texto: str = '') -> str:
     
     return opc
 
-def guardar_ids_de_carpetas(file_ids: dict, carpetas:dict):
+def guardar_ids_de_elementos(elementos_ids: dict, elementos:dict):
     
-    #voy a facilitar la funcion suponiendo q no hay 2 carpetas con el mismo nomrbe, 
+    #voy a facilitar la funcion suponiendo q no hay 2 carpetas o archivos con el mismo nomrbe, 
     #asi puedo buscar las carpeta directamente por el nombre q me indica el usuario
 
-    for carpeta in carpetas:
+    for elemento in elementos:
         
-        file_ids[ carpeta['name'] ] = carpeta['id']        
+        elementos_ids[ elemento['name'] ] = elemento['id']        
         #file_ids[ carpeta['id'] ] = carpeta['name']
 
-
 #LISTAR TODAS LAS CARPETAS EN EL REMOTO
+
 def listar_todas_carpetas() -> None:
     """
     PRE: 
     POST: Muestra por pantalla todas las carpetas disponibles en drive y devuelve un 
     diccionario con todos los nombres de las carpetas y sus respectivos 'ids'
     """
-    print('CARPETAS DISPONIBLES EN EL REMOTO\n')
+    
     page_token = None
     cortar = False
     resultados_tot = 0
-    file_ids = dict()
+    elementos_ids = dict() #elementos pueden ser carpetas o archivos
     while not cortar:
         #traigo resultados
         #ACLARACIONES IMPORTANTES QUIZA OBVIAS PERO ME COSTARON:
@@ -53,7 +53,7 @@ def listar_todas_carpetas() -> None:
         #en el dict files, accedo a la clave 'name'
 
         carpetas = resultados['files']
-        guardar_ids_de_carpetas(file_ids, carpetas)
+        guardar_ids_de_elementos(elementos_ids, carpetas)
         
         for carpeta in carpetas:
             print (carpeta['name'])
@@ -66,38 +66,44 @@ def listar_todas_carpetas() -> None:
     
     print(f'\nResultados totales: {resultados_tot}')
     
-    return file_ids
+    return folder_ids
 
-file_ids = listar_todas_carpetas()
+#file_ids = listar_todas_carpetas()
+# print(file_ids)
 
-def busqueda_personalizada():
+
+def listar_elementos(query):
     """
     PRE: 
     POST: 
     """
     print('BUSCADOR DE ARCHIVOS DE DRIVE\n')
-    query = input= ('')
+    #searchFile(5,"name contains 'prueba' and fullText contains 'prueba' and mimeType = 'text/plain'")
+    #searchFile(3,"name contains 'prueba' and mimeType = 'text/plain' ")
+    # palabra = input ('ingerse palabra de busqueda clave: ')
+    # query = f"name contains '{palabra}'"
+
     page_token = None
     cortar = False
     resultados_tot = 0
-    file_ids = dict()
+    elementos_ids = dict()
     while not cortar:
         #traigo resultados
         #ACLARACIONES IMPORTANTES QUIZA OBVIAS PERO ME COSTARON:
         #.list() DEVUELVE UN DICCIONARIO DE DICCIONARIOS, DONDE UNA DE LAS CLAVES ES files(), 
         #q a su vez es un diccionario.
-        resultados = service().files().list(q="mimeType= 'application/vnd.google-apps.folder'",
+        resultados = service().files().list(q= query,
                                             spaces='drive',
                                             fields='nextPageToken, files(id, name)',
                                             pageToken=page_token).execute()
         #print(resultados)
         #en el dict files, accedo a la clave 'name'
 
-        carpetas = resultados['files']
-        guardar_ids_de_carpetas(file_ids, carpetas)
+        elementos = resultados['files']
+        guardar_ids_de_elementos(elementos_ids, elementos)
         
-        for carpeta in carpetas:
-            print (carpeta['name'])
+        for elemento in elementos:
+            print (elemento['name'])
             resultados_tot += 1
 
         #chequeo si hay mas resultados
@@ -107,22 +113,42 @@ def busqueda_personalizada():
     
     print(f'\nResultados totales: {resultados_tot}')
     
-    return file_ids
+    return elementos_ids
 
-'''
+
+def armado_de_sentencia_consulta() -> str:
+    """
+    PRE:
+    POST: devuelve el string 'query' con la consulta a bucar en drive
+    """
+    print('1-Busqueda manual (lista carpetas disponibles)\n2-Busqueda personalizada')
+    opc = int(validar_opcion(1,2))
+    if opc == 1:
+        query ="mimeType= 'application/vnd.google-apps.folder'" #para buscar carpetas
+    else:
+        print('Que desea buscar?\n 1-Carpetas\n2-Archivos')
+        opc = int(validar_opcion(1,2))
+        if opc == 1:
+            palabra = input ('ingerse palabra clave de busqueda: ')
+            query = f"name contains '{palabra}' and mimeType = 'application/vnd.google-apps.folder'"
+        # else:
+        #     query = 
+    
+    return query
+
 def consultar_archivos():
     print('CONSULTAR ARCHIVOS DE DRIVE\n')
-    print('1- Listar carpetas disponibles\n 2-Busqueda personalizada\n')
-    opc = validar_opcion(1,2)
-    if opc == 1:
-        listar_todas_carpetas()
-    elif:
-        busqueda_personalizada()
-
-'''
+    
+    query= armado_de_sentencia_consulta()
+    #print('CARPETAS DISPONIBLES EN EL REMOTO\n')
+    
+    elementos_ids = listar_elementos(query)
+    print(elementos_ids)
+    
+    #file_ids = busqueda_personalizada()
+    #print(file_ids)
         
-
-
+consultar_archivos()
 
 #print(file_ids)
 
