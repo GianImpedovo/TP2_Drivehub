1d70f20cdb2813a06d6ba9e1c49ace122eb35875
gianfranco <impefranco98@gmail.com>
2021-07-19 13:54:16 -0300

    agrego sistema para recorrer ficheros

--- funcionalidad_carpetas.py
+++ funcionalidad_carpetas.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 RUTA = os.getcwd()
-print(RUTA)
+
 
 
 # Necesito recibir las evaluaciones de los alumnos por gmail
@@ -55,4 +55,48 @@ def crear_carpeta_evaluacion():
 
     crear_carpeta_profesores(archivo_docente_alumno, nombre_ev)
 
-crear_carpeta_evaluacion()
+#crear_carpeta_evaluacion()
+
+# ----------- recorrido de carpetas 
+
+def mostrar_directorio_actual(ruta_actual: str)->None:
+    '''
+    PRE: Recibo la ruta donde me encuntro
+    POST: Retorno un listado de los elementos que se encuentran en donde estoy parado
+    '''
+    print(ruta_actual)
+    contenido = os.listdir(ruta_actual)
+    for fichero in contenido:
+        if os.path.isfile(ruta_actual + "/" + fichero):
+            print("Archivo - ",fichero)
+        else:
+            print("Carpeta - ",fichero)
+            contenido_carpeta = os.listdir(ruta_actual + "/" + fichero)
+
+            for contenido in contenido_carpeta:
+                if os.path.isdir(ruta_actual + "/" + fichero + "/" + contenido):
+                    print(" -> carpeta: ", contenido)
+                else:
+                    print(" -> archivo:  ",contenido)
+
+def recorrer_directorio(ruta_actual: str)->None:
+    mostrar_directorio_actual(ruta_actual)
+    print(ruta_actual)
+    for dirpath, dirnames, filenames in os.walk(ruta_actual):
+        ", ".join(dirnames)
+        ", ".join(filenames)
+
+        print("\n ---> Recorrer directorio , si desea salir escriba 'salir'")
+        accion = input("A que directorio/archivo desea ir: ")
+        if accion == "salir":
+            ruta_actual = "/home/gianfranco/Documentos/fiuba/Algoritmos/TP2" # cambiar por la ruta del usuario
+            break # me lleva al main
+        elif accion == "mover":
+            if os.path.isdir(ruta_actual):
+                recorrer_directorio(ruta_actual + "/" + accion)
+            else:
+                pass
+                # abrir el archivo
+
+
+recorrer_directorio(RUTA)
